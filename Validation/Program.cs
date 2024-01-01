f32b7ac82b28f47c2461fabecbd1a7a36bdd3a70
beretnash <beretnash@gmail.com>
2024-01-01 20:55:27 +0200

    implement main method body

--- Validation/Program.cs
+++ Validation/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace Validation
+﻿using Validation.Interfaces;
+
+namespace Validation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            if (args?.Length < 2)
+            {
+                Console.WriteLine("ERROR: Bad amount of params.");
+                return;
+            }
+
+            var validationType = args[0];
+            var validationValue = args[1];
+
+            IValidatorCreator validatorCreator = new ValidatorCreator();
+
+            try
+            {
+                IValidator validator = validatorCreator.CreateValidator(validationType);
+                var result = validator.Execute(validationValue);
+                Console.WriteLine(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
         }
     }
 }
