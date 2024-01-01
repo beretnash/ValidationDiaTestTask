9e89f145312c37dd5189c0039ef546a66c0d9036
beretnash <beretnash@gmail.com>
2024-01-01 20:05:06 +0200

    implement rnokpp validation

---
+++ Validation.UnitTests/Validators/IndividualTaxNumberValidatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validation.Validators;
+
+namespace Validation.UnitTests.Validators
+{
+    public class IndividualTaxNumberValidatorTests
+    {
+        [Test]
+        [TestCase("1122334455", ExpectedResult = true)]
+        [TestCase("1234567489", ExpectedResult = true)]
+        [TestCase("2143658791", ExpectedResult = true)]
+        public bool Execute_ValidPassportNumber_ReturnsTrue(string passportNumber)
+        {
+            // Arrange
+            IndividualTaxNumberValidator individualTaxNumberValidator = new IndividualTaxNumberValidator();
+
+            // Act
+            bool result = individualTaxNumberValidator.Execute(passportNumber);
+
+            // Assert
+            return result;
+        }
+
+        [Test]
+        [TestCase("112233445", ExpectedResult = false)]
+        [TestCase("11223344551", ExpectedResult = false)]
+        [TestCase("112233445F", ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase(null, ExpectedResult = false)]
+        public bool Execute_InvalidPassportNumber_ReturnsFalse(string passportNumber)
+        {
+            // Arrange
+            IndividualTaxNumberValidator individualTaxNumberValidator = new IndividualTaxNumberValidator();
+
+            // Act
+            bool result = individualTaxNumberValidator.Execute(passportNumber);
+
+            // Assert
+            return result;
+        }
+    }
+}
--- Validation/Validators/IndividualTaxNumberValidator.cs
+++ Validation/Validators/IndividualTaxNumberValidator.cs
@@ -1,4 +1,5 @@
-﻿using Validation.Interfaces;
+﻿using System.Text.RegularExpressions;
+using Validation.Interfaces;
 
 namespace Validation.Validators
 {
@@ -6,7 +7,8 @@ namespace Validation.Validators
     {
         public bool Execute(string value)
         {
-            return false;
+            var pattern = @"\d{10}";
+            return !string.IsNullOrEmpty(value) && value.Length == 10 && Regex.IsMatch(value, pattern);
         }
     }
 }
